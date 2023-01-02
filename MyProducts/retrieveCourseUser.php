4f9c1c953e9206c633ddd80003e135bdf4943542
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-01-02 20:44:05 +0100

    Update retrieveCourseUser.php

--- MyProducts/retrieveCourseUser.php
+++ MyProducts/retrieveCourseUser.php
@@ -8,20 +8,18 @@
     if(!$mysqli)
         die("connction failed " . mysqli_connect_error());
     $user_id = $_SESSION['userId'];
-    $sql = "select * from userclient u join commande c on u.userId = c.userid join contient co on c.idCmd = co.idCmd join course on co.idCourse = course.courseId where u.userId = $user_id and etat = 1";
+    $sql = "select * from listeproduit join userclient on listeproduit.userId = userclient.userId join course on listeproduit.courseId = course.courseId where userclient.userId = 
+    $user_id";
     // Select all rows from the table
     $result = mysqli_query($mysqli, $sql);
     // Fetch the rows one by one
 
    if(mysqli_num_rows($result) > 0)
    {
-       
         while ($row = mysqli_fetch_assoc($result))
             $data[] = $row;
         $json_data = json_encode($data);
-       
         echo $json_data;        
-       
    } else echo "";
     //close connection : 
     mysqli_close($mysqli);
